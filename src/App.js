fcbdf9c7bc126a7dd45945de740da507e4d80318
l1ttlestar <michelle020517@gmail.com>
2022-05-19 12:09:32 +0900

    07까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-function Potato() {
-  return <h3>i love Potato</h3> 
+import axios from "axios";
+import React from "react";
+import Movie from "./Movie";
+import './App.css';
 
+class App extends React.Component {
+  state = {
+    isLoadig: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading</span>
+          </div>
+
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
-
-function App() {
-  return (
-  <div>
-    <h1>Hello</h1>
-    <Potato />
-  </div> 
-  );
-}
-
 export default App;
+
